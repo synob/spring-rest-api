202637a5e76cc03cec55f38872c7656517959437
syno <nogary11@gmail.com>
2020-04-20 17:21:27 +0900

    3-1. @ApiOperation  어노테이션 컨트롤러에 추가

--- src/main/java/com/rest/api/controller/v1/UserController.java
+++ src/main/java/com/rest/api/controller/v1/UserController.java
@@ -8,10 +8,15 @@ import com.rest.api.repo.UserJpaRepo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 
+@Api(tags = {"1. User"})
 @RequiredArgsConstructor
 @RequestMapping(value = "/v1")
 @RestController
@@ -19,13 +24,16 @@ public class UserController {
 
   private final UserJpaRepo userJpaRepo;
 
+  @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다")
   @GetMapping(value = "/user")
   public List<User> findAllUser(){
     return userJpaRepo.findAll();
   }
 
+  @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
   @PostMapping(value = "/user")
-  public User save(){
+  public User save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
+  @ApiParam(value = "회원이름", required = true) @RequestParam String name){
     User user = User.builder()
     .uid("syno@kt.com")
     .name("syno").build();
